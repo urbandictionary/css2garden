bb673b7bfee1c2c9916a60e2cbdb513b89e70f3c
Aaron Peckham <apeckham@gmail.com>
2019-08-27 12:59:52 -0400

    initial

--- css/test.cljs
+++ css/test.cljs
@@ -1,5 +1,23 @@
 (ns css.test
-  (:require [clojure.test :refer [deftest is]]))
+  (:require [clojure.test :refer [deftest is]]
+            [css :as js-css]
+            [clojure.walk :refer [postwalk]]))
 
-(deftest my-test
-  (is (= 1 1)))
+(defn remove-position
+  [item]
+  (if (map? item) (dissoc item :position :source) item))
+
+(defn parse
+  [input]
+  (get-in (postwalk remove-position
+                    (-> input
+                        js-css/parse
+                        (js->clj :keywordize-keys true)))
+          [:stylesheet :rules]))
+
+(deftest parse-test
+  (is (= [{:type "rule",
+           :selectors ["body"],
+           :declarations
+             [{:type "declaration", :property "font-size", :value "12px"}]}]
+         (parse "body { font-size: 12px; }"))))
