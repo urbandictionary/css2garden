38e9a84b00ffa4a231e7151d2672d9ce69f7083e
Aaron Peckham <apeckham@gmail.com>
2019-08-27 14:49:30 -0400

    simplify test

--- src/css/garden_test.cljs
+++ src/css/garden_test.cljs
@@ -1,16 +1,17 @@
 (ns css.garden-test
   (:require [clojure.test :refer [deftest is]]
-            [garden.core :as garden]))
+            [garden.core :as garden]
+            [garden.stylesheet :refer [at-media]]))
+
+(defn compile [input] (garden/css {:pretty-print? false} input))
 
 (deftest garden-test
-  (is (= "body {\n  font-size: 18px;\n}"
-         (garden/css [:body {:font-size "18px"}])))
-  (is (= "body h1 {\n  font-size: 18px;\n}"
-         (garden/css [:body [:h1 {:font-size "18px"}]])))
-  (is
-    (= "body h1 {\n  font-size: 18px;\n}\n\nbody h2 {\n  font-size: 18px;\n}"
-       (garden/css [:body [:h1 {:font-size "18px"}] [:h2 {:font-size "18px"}]])
-       (garden/css [:body [:h1 {:font-size "18px"}] :body
-                    [:h2 {:font-size "18px"}]])))
-  (is (= "body, h1 {\n  font-size: 18px;\n}"
-         (garden/css [:body :h1 {:font-size "18px"}]))))
+  (is (= "body{font-size:18px}" (compile [:body {:font-size "18px"}])))
+  (is (= "body h1{font-size:18px}" (compile [:body [:h1 {:font-size "18px"}]])))
+  (is (= "body h1{font-size:18px}body h2{font-size:18px}"
+         (compile [:body [:h1 {:font-size "18px"}] [:h2 {:font-size "18px"}]])
+         (compile [:body [:h1 {:font-size "18px"}] :body
+                   [:h2 {:font-size "18px"}]])))
+  (is (= "body,h1{font-size:18px}" (compile [:body :h1 {:font-size "18px"}])))
+  (is (= "@media screen{h1{a:b}}"
+         (compile [(at-media {:screen true} [:h1 {:a :b}])]))))
--- src/css/run_test.cljs
+++ src/css/run_test.cljs
@@ -13,6 +13,21 @@
          (map :selectors (parse "body, h1 { font-size: 12px; }"))))
   (is (= [["body h1" "body h2"]]
          (map :selectors (parse "body h1, body h2 { font-size: 12px; }"))))
+  (is
+    (=
+      [{:type "media",
+        :media "screen and (max-width: 992px)",
+        :rules [{:type "rule",
+                 :selectors ["body"],
+                 :declarations [{:type "declaration",
+                                 :property "background-color",
+                                 :value "blue"}]}]}]
+      (parse
+        "@media screen and (max-width: 992px) {
+          body {
+            background-color: blue;
+          }
+        }")))
   (is (= [["body h1" "h2"]]
          (map :selectors (parse "body h1, h2 { font-size: 12px; }")))))
 
