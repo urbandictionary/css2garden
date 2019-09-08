abc59bc0c40cca9ae3834cb010532d1725e767e5
Aaron Peckham <apeckham@gmail.com>
2019-09-07 20:30:30 -0700

    refactor test

--- src/css2garden/css_to_ast_test.cljs
+++ src/css2garden/css_to_ast_test.cljs
@@ -1,5 +1,5 @@
 (ns css2garden.css-to-ast-test
-  (:require [clojure.test :refer [deftest is testing]]
+  (:require [clojure.test :refer [deftest is testing are]]
             [css2garden.core :refer [css->ast]]))
 
 (deftest css->ast-test
@@ -10,16 +10,17 @@
              :declarations
                [{:type "declaration", :property "font-size", :value "12px"}]}]
            (css->ast "body { font-size: 12px; }"))))
-  (testing
-    "selectors"
-    (is (= [["body h1"]]
-           (map :selectors (css->ast "body h1 { font-size: 12px; }"))))
-    (is (= [["body" "h1"]]
-           (map :selectors (css->ast "body, h1 { font-size: 12px; }"))))
-    (is (= [["body h1" "body h2"]]
-           (map :selectors (css->ast "body h1, body h2 { font-size: 12px; }"))))
-    (is (= [["body h1" "h2"]]
-           (map :selectors (css->ast "body h1, h2 { font-size: 12px; }")))))
+  (testing "selectors"
+           (are [selectors css]
+                (= selectors (map :selectors (css->ast css)))
+                [["body h1"]]
+                "body h1 { }"
+                [["body" "h1"]]
+                "body, h1 { }"
+                [["body h1" "body h2"]]
+                "body h1, body h2 { }"
+                [["body h1" "h2"]]
+                "body h1, h2 { }"))
   (testing
     "media query"
     (is
