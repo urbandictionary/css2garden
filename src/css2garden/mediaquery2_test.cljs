5ba259158c58a5b65adbf93ed3d773373ff0446d
Aaron Peckham <apeckham@gmail.com>
2019-09-07 21:47:25 -0700

    fix repl; remove redundant test

--- src/css2garden/mediaquery2_test.cljs
+++ src/css2garden/mediaquery2_test.cljs
@@ -32,35 +32,4 @@
                           :nodes [{:type :media-feature, :value "min-width"}
                                   {:type :colon, :value ":"}
                                   {:type :value, :value "600px"}]}]}]}
-       (parse "screen and (max-width: 900px) and (min-width: 600px)")))
-  (is
-    (=
-      {:type :media-query-list,
-       :value
-         "screen and (min-device-width:1080px) and (orientation:portrait) and (-webkit-min-device-pixel-ratio:3)",
-       :nodes
-         [{:type :media-query,
-           :value
-             "screen and (min-device-width:1080px) and (orientation:portrait) and (-webkit-min-device-pixel-ratio:3)",
-           :nodes [{:type :media-type, :value "screen"}
-                   {:type :keyword, :value "and"}
-                   {:type :media-feature-expression,
-                    :value "(min-device-width:1080px)",
-                    :nodes [{:type :media-feature, :value "min-device-width"}
-                            {:type :colon, :value ":"}
-                            {:type :value, :value "1080px"}]}
-                   {:type :keyword, :value "and"}
-                   {:type :media-feature-expression,
-                    :value "(orientation:portrait)",
-                    :nodes [{:type :media-feature, :value "orientation"}
-                            {:type :colon, :value ":"}
-                            {:type :value, :value "portrait"}]}
-                   {:type :keyword, :value "and"}
-                   {:type :media-feature-expression,
-                    :value "(-webkit-min-device-pixel-ratio:3)",
-                    :nodes [{:type :media-feature,
-                             :value "-webkit-min-device-pixel-ratio"}
-                            {:type :colon, :value ":"}
-                            {:type :value, :value "3"}]}]}]}
-      (parse
-        "screen and (min-device-width:1080px) and (orientation:portrait) and (-webkit-min-device-pixel-ratio:3)"))))
+       (parse "screen and (max-width: 900px) and (min-width: 600px)"))))
