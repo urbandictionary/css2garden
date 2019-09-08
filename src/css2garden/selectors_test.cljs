3f63b638feb52f6547bbf63bf6739a2a8fd688dc
Aaron Peckham <apeckham@gmail.com>
2019-09-08 14:50:44 -0700

    use ast->clj

--- src/css2garden/selectors_test.cljs
+++ src/css2garden/selectors_test.cljs
@@ -1,22 +1,23 @@
 (ns css2garden.selectors-test
   (:require css-what
+            [css2garden.ast :refer [ast->clj]]
             [clojure.test :refer [deftest is are]]))
 
-(defn parse [input] (js->clj (css-what/parse input) :keywordize-keys true))
+(defn parse [input] (ast->clj (css-what/parse input)))
 
 (deftest parse-test
-  (is (= [[{:type "tag", :name "a"}
-           {:type "attribute",
+  (is (= [[{:type :tag, :name "a"}
+           {:type :attribute,
             :name "b",
             :action "exists",
             :value "",
-            :ignoreCase false} {:type "pseudo", :name "c", :data nil}
-           {:type "descendant"} {:type "tag", :name "d"} {:type "child"}
-           {:type "tag", :name "e"}]
-          [{:type "tag", :name "f"}
-           {:type "attribute",
+            :ignoreCase false} {:type :pseudo, :name "c", :data nil}
+           {:type :descendant} {:type :tag, :name "d"} {:type :child}
+           {:type :tag, :name "e"}]
+          [{:type :tag, :name "f"}
+           {:type :attribute,
             :name "x",
             :action "equals",
             :value "y",
-            :ignoreCase false}] [{:type "tag", :name "g"}]]
+            :ignoreCase false}] [{:type :tag, :name "g"}]]
          (parse "a[b]:c d>e,f[x=y],g"))))
