89dc78e2e502eb71a99ffe6a1fb4ac53c7ad44b5
Aaron Peckham <apeckham@gmail.com>
2019-09-07 20:28:04 -0700

    add notes to test

--- src/css2garden/css_to_ast_test.cljs
+++ src/css2garden/css_to_ast_test.cljs
@@ -3,31 +3,37 @@
             [css2garden.core :refer [css->ast]]))
 
 (deftest css->ast-test
-  (is (= [{:type "rule",
-           :selectors ["body"],
-           :declarations
-             [{:type "declaration", :property "font-size", :value "12px"}]}]
-         (css->ast "body { font-size: 12px; }")))
-  (is (= [["body h1"]]
-         (map :selectors (css->ast "body h1 { font-size: 12px; }"))))
-  (is (= [["body" "h1"]]
-         (map :selectors (css->ast "body, h1 { font-size: 12px; }"))))
-  (is (= [["body h1" "body h2"]]
-         (map :selectors (css->ast "body h1, body h2 { font-size: 12px; }"))))
-  (is
-    (=
-      [{:type "media",
-        :media "screen and (max-width: 992px)",
-        :rules [{:type "rule",
-                 :selectors ["body"],
-                 :declarations [{:type "declaration",
-                                 :property "background-color",
-                                 :value "blue"}]}]}]
-      (css->ast
-        "@media screen and (max-width: 992px) {
+  (testing
+    "basic"
+    (is (= [{:type "rule",
+             :selectors ["body"],
+             :declarations
+               [{:type "declaration", :property "font-size", :value "12px"}]}]
+           (css->ast "body { font-size: 12px; }"))))
+  (testing
+    "selectors"
+    (is (= [["body h1"]]
+           (map :selectors (css->ast "body h1 { font-size: 12px; }"))))
+    (is (= [["body" "h1"]]
+           (map :selectors (css->ast "body, h1 { font-size: 12px; }"))))
+    (is (= [["body h1" "body h2"]]
+           (map :selectors (css->ast "body h1, body h2 { font-size: 12px; }"))))
+    (is (= [["body h1" "h2"]]
+           (map :selectors (css->ast "body h1, h2 { font-size: 12px; }")))))
+  (testing
+    "media query"
+    (is
+      (=
+        [{:type "media",
+          :media "screen and (max-width: 992px)",
+          :rules [{:type "rule",
+                   :selectors ["body"],
+                   :declarations [{:type "declaration",
+                                   :property "background-color",
+                                   :value "blue"}]}]}]
+        (css->ast
+          "@media screen and (max-width: 992px) {
           body {
             background-color: blue;
           }
-        }")))
-  (is (= [["body h1" "h2"]]
-         (map :selectors (css->ast "body h1, h2 { font-size: 12px; }")))))
+        }")))))
