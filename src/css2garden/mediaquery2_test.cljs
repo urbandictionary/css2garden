8fa19230a9174b7476f5048bb5900b52cf8dc5e6
Aaron Peckham <apeckham@gmail.com>
2019-09-07 22:01:38 -0700

    try to convert ast to garden

--- src/css2garden/mediaquery2_test.cljs
+++ src/css2garden/mediaquery2_test.cljs
@@ -1,5 +1,6 @@
 (ns css2garden.mediaquery2-test
   (:require [clojure.test :refer [deftest is are]]
+            [clojure.walk :refer [postwalk]]
             postcss-media-query-parser))
 
 (defn node->clj
@@ -33,3 +34,31 @@
                                   {:type :colon, :value ":"}
                                   {:type :value, :value "600px"}]}]}]}
        (parse "screen and (max-width: 900px) and (min-width: 600px)"))))
+
+(defmulti visitor :type)
+
+(defn media-feature-map
+  [nodes]
+  (reduce (fn [accum {:keys [type value]}] (assoc accum type value)) {} nodes))
+
+(defmethod visitor :media-feature-expression
+  [{:keys [nodes]}]
+  (let [{:keys [media-feature value]} (media-feature-map nodes)]
+    {(keyword media-feature) value}))
+
+(defmethod visitor :media-query
+  [{:keys [nodes]}]
+  (apply merge (remove #(#{"and"} (:value %)) nodes)))
+
+(defmethod visitor :media-type [{:keys [value]}] {(keyword value) true})
+
+(defmethod visitor :default [node] node)
+
+(defmethod visitor :media-query-list [{:keys [nodes]}] nodes)
+
+(defn ast->garden [ast] (postwalk visitor ast))
+
+(deftest ast->garden-test
+  (is (= [{:screen true, :max-width "900px", :min-width "600px"}]
+         (ast->garden
+           (parse "screen and (max-width: 900px) and (min-width: 600px)")))))
