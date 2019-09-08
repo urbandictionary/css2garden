67962800e0e5d0ca05085db954aaed9924498af3
Aaron Peckham <apeckham@gmail.com>
2019-09-08 14:39:43 -0700

    add example of css-what

--- src/css2garden/selectors_test.cljs
+++ src/css2garden/selectors_test.cljs
@@ -1,4 +1,20 @@
-(ns css2garden.selectors-test)
+(ns css2garden.selectors-test
+  (:require [css-what :refer [parse]]
+            [clojure.test :refer [deftest is are]]))
 
-; (require '[css-what :refer [parse]])
-; (js->clj (parse "foo[bar]:baz") :keywordize-keys true)
+(deftest parse-test
+  (is (= [[{:type "tag", :name "a"}
+           {:type "attribute",
+            :name "b",
+            :action "exists",
+            :value "",
+            :ignoreCase false} {:type "pseudo", :name "c", :data nil}
+           {:type "descendant"} {:type "tag", :name "d"} {:type "child"}
+           {:type "tag", :name "e"}]
+          [{:type "tag", :name "f"}
+           {:type "attribute",
+            :name "x",
+            :action "equals",
+            :value "y",
+            :ignoreCase false}] [{:type "tag", :name "g"}]]
+         (js->clj (parse "a[b]:c d>e,f[x=y],g") :keywordize-keys true))))
