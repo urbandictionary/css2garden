73872e478ba4130e037cfee1345d87f26515876d
Aaron Peckham <apeckham@gmail.com>
2019-09-07 21:34:21 -0700

    add more examples

--- src/css2garden/mediaquery2_test.cljs
+++ src/css2garden/mediaquery2_test.cljs
@@ -15,6 +15,25 @@
 
 (deftest postcss-media-query-parser-test
   (is
+    (= {:type :media-query-list,
+        :value "screen and (max-width: 900px) and (min-width: 600px)",
+        :nodes [{:type :media-query,
+                 :value "screen and (max-width: 900px) and (min-width: 600px)",
+                 :nodes [{:type :media-type, :value "screen"}
+                         {:type :keyword, :value "and"}
+                         {:type :media-feature-expression,
+                          :value "(max-width: 900px)",
+                          :nodes [{:type :media-feature, :value "max-width"}
+                                  {:type :colon, :value ":"}
+                                  {:type :value, :value "900px"}]}
+                         {:type :keyword, :value "and"}
+                         {:type :media-feature-expression,
+                          :value "(min-width: 600px)",
+                          :nodes [{:type :media-feature, :value "min-width"}
+                                  {:type :colon, :value ":"}
+                                  {:type :value, :value "600px"}]}]}]}
+       (parse "screen and (max-width: 900px) and (min-width: 600px)")))
+  (is
     (=
       {:type :media-query-list,
        :value
