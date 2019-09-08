7da04c55cde43d4fbdea34fef9da0e39a421f532
Aaron Peckham <apeckham@gmail.com>
2019-09-07 20:43:21 -0700

    add tests for at-media

--- src/css2garden/garden_to_css_test.cljs
+++ src/css2garden/garden_to_css_test.cljs
@@ -1,6 +1,7 @@
 (ns css2garden.garden-to-css-test
   (:require [clojure.test :refer [deftest is are]]
             [garden.core :as garden]
+            [garden.units :refer [px]]
             [garden.stylesheet :refer [at-media]]))
 
 (defn garden->css [input] (garden/css {:pretty-print? false} input))
@@ -16,3 +17,14 @@
          [:body [:h1 {:font-size "18px"}] :body [:h2 {:font-size "18px"}]]
        "body,h1{font-size:18px}" [:body :h1 {:font-size "18px"}]
        "@media screen{h1{a:b}}" [(at-media {:screen true} [:h1 {:a :b}])]))
+
+(deftest at-media-test
+  (is (= "" (at-media {:screen true} [])))
+  (is (= "@media screen{}" (garden->css (at-media {:screen true} []))))
+  (is (= "@media not screen{}" (garden->css (at-media {:screen false} []))))
+  (is (= "@media screen and not braille{}"
+         (garden->css (at-media {:screen true, :braille false} []))))
+  (is (= "@media only screen{}" (garden->css (at-media {:screen :only} []))))
+  (is (= "@media(min-width:768px) and (max-width:959px){}"
+         (garden->css (at-media {:min-width (px 768), :max-width (px 959)}
+                                [])))))
