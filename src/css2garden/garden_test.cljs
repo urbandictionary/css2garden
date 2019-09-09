6ac67567ffbd1e3f3a34207bb7fe17840e34e4bb
Aaron Peckham <apeckham@gmail.com>
2019-09-08 17:15:31 -0700

    more examples

--- src/css2garden/garden_test.cljs
+++ src/css2garden/garden_test.cljs
@@ -2,6 +2,7 @@
   (:require [clojure.test :refer [deftest is are testing]]
             [garden.core :as garden]
             [garden.units :refer [px]]
+            [garden.selectors :as s]
             [garden.stylesheet :refer [at-media]]))
 
 (defn garden->css [input] (garden/css {:pretty-print? false} input))
@@ -18,6 +19,9 @@
          "body h1{font-size:18px}body h2{font-size:18px}"
            [:body [:h1 {:font-size "18px"}] :body [:h2 {:font-size "18px"}]]
          "body,h1{font-size:18px}" [:body :h1 {:font-size "18px"}]
+         ":not(.important.dialog){font-size:18px}" [(s/not :.important.dialog)
+                                                    {:font-size "18px"}]
+         "p:lang(it){font-size:18px}" [(s/p (s/lang :it)) {:font-size "18px"}]
          "@media screen{h1{a:b}}" [(at-media {:screen true} [:h1 {:a :b}])])))
 
 (deftest at-media-test
