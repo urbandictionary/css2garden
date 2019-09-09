0c4872dba5b55fe2840cb29f87a9e251d4863e03
Aaron Peckham <apeckham@gmail.com>
2019-09-08 20:45:28 -0700

    use mediaquery in postcss-test

--- src/css2garden/postcss_test.cljs
+++ src/css2garden/postcss_test.cljs
@@ -1,5 +1,6 @@
 (ns css2garden.postcss-test
   (:require [clojure.test :refer [deftest is are]]
+            [css2garden.mediaquery :as mq]
             [postcss :refer [parse]]
             [clojure.walk :refer [postwalk]]
             [css2garden.ast :refer [ast->clj]]))
@@ -61,7 +62,7 @@
 
 (defmulti visit :type)
 
-(defmethod visit :root [{:keys [nodes]}] (first nodes))
+(defmethod visit :root [{:keys [nodes]}] nodes)
 
 (defmethod visit :rule
   [{:keys [selector nodes]}]
@@ -70,23 +71,50 @@
      {}
      nodes)])
 
+(defmethod visit :atrule
+  [{:keys [params nodes]}]
+  (list 'at-media (mq/ast->garden (mq/parse params)) (apply concat nodes)))
+
 (defmethod visit :default [ast] ast)
 
 (defn ast->garden [ast] (postwalk visit ast))
 
 (deftest ast->garden-test
-  (is (= ["body" {:font-size "12px"}]
+  (is (= [["body" {:font-size "12px"}]]
          (-> "body {font-size: 12px}"
              parse
              ast->clj
              ast->garden)))
-  (is (= ["body" {:font-size "12px", :font-weight "bold"}]
+  (is (= [["body" {:font-size "12px", :font-weight "bold"}]]
          (-> "body {font-size: 12px; font-weight: bold}"
              parse
              ast->clj
              ast->garden)))
-  (is (= ["body, h1, h2" {:font-size "12px", :font-weight "bold"}]
+  (is (= [["body, h1, h2" {:font-size "12px", :font-weight "bold"}]]
          (-> "body, h1, h2 {font-size: 12px; font-weight: bold}"
              parse
              ast->clj
-             ast->garden))))
+             ast->garden)))
+  (is
+    (=
+      [(list 'at-media
+             {:screen false, :max-height "300px"}
+             ["h1" {:font-size "12px"} "h2" {:font-weight "bold"}])
+       ["h3" {:font-style "italic"}]]
+      (->
+        "
+          @media not screen and (max-height: 300px) {
+            h1 {
+              font-size: 12px;
+            }
+            h2 {
+              font-weight: bold;
+            }
+          }
+          h3 {
+            font-style: italic;
+          }
+        "
+        parse
+        ast->clj
+        ast->garden))))
