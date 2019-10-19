64b4db27d118e8b65042fe44c1aa6c9e86a91b6d
Aaron <apeckham@gmail.com>
2019-10-18 23:58:47 -0700

    remove "are"

--- src/css2garden/garden_test.cljs
+++ src/css2garden/garden_test.cljs
@@ -1,45 +1,39 @@
 (ns css2garden.garden-test
-  (:require [clojure.test :refer [deftest is are testing]]
-            [garden.core :as garden]
-            [garden.units :refer [px]]
-            [garden.selectors :as s]
-            [garden.stylesheet :refer [at-media]]))
+  (:require
+   [clojure.test :refer [deftest is are testing]]
+   [garden.core :as garden]
+   [garden.units :refer [px]]
+   [garden.selectors :as s]
+   [garden.stylesheet :refer [at-media]]))
 
 (defn garden->css [input] (garden/css {:pretty-print? false} input))
 
 (deftest css-test
-  (testing
-    "garden's own css features"
-    (are [css garden]
-         (= css (garden->css garden))
-         "body{font-size:18px}" [:body {:font-size "18px"}]
-         "body h1{font-size:18px}" [:body [:h1 {:font-size "18px"}]]
-         "body h1{font-size:18px}body h2{font-size:18px}"
-           [:body [:h1 {:font-size "18px"}] [:h2 {:font-size "18px"}]]
-         "body h1{font-size:18px}body h2{font-size:18px}"
-           [:body [:h1 {:font-size "18px"}] :body [:h2 {:font-size "18px"}]]
-         "body,h1{font-size:18px}" [:body :h1 {:font-size "18px"}]
-         "a:not(.internal){}" [(s/a (s/not :.internal)) {}]
-         "li:nth-child(2n+3){}" [(s/li (s/nth-child "2n+3")) {}]
-         ".alpha::first-letter,.bravo::first-line{}"
-           [((s/selector :.alpha) s/first-letter)
-            ((s/selector :.bravo) s/first-line) {}]
-         ":not(.important.dialog){font-size:18px}" [(s/not :.important.dialog)
-                                                    {:font-size "18px"}]
-         "p:lang(it){font-size:18px}" [(s/p (s/lang :it)) {:font-size "18px"}]
-         "funny-selector:lang(urr) > b{font-size:18px}"
-           ["funny-selector:lang(urr) > b" {:font-size "18px"}]
-         "@media screen{h1{a:b}}" [(at-media {:screen true} [:h1 {:a :b}])])))
+  (testing "garden's own css features"
+           (is (= "body{font-size:18px}" (garden->css [:body {:font-size "18px"}])))
+           (is (= "body h1{font-size:18px}" (garden->css [:body [:h1 {:font-size "18px"}]])))
+           (is (= "body h1{font-size:18px}body h2{font-size:18px}"
+                  (garden->css [:body [:h1 {:font-size "18px"}] [:h2 {:font-size "18px"}]])))
+           (is (= "body h1{font-size:18px}body h2{font-size:18px}"
+                  (garden->css [:body [:h1 {:font-size "18px"}] :body [:h2 {:font-size "18px"}]])))
+           (is (= "body,h1{font-size:18px}" (garden->css [:body :h1 {:font-size "18px"}])))
+           (is (= "a:not(.internal){}" (garden->css [(s/a (s/not :.internal)) {}])))
+           (is (= "li:nth-child(2n+3){}" (garden->css [(s/li (s/nth-child "2n+3")) {}])))
+           (is (= ".alpha::first-letter,.bravo::first-line{}"
+                  (garden->css [((s/selector :.alpha) s/first-letter) ((s/selector :.bravo) s/first-line) {}])))
+           (is (= ":not(.important.dialog){font-size:18px}"
+                  (garden->css [(s/not :.important.dialog) {:font-size "18px"}])))
+           (is (= "p:lang(it){font-size:18px}" (garden->css [(s/p (s/lang :it)) {:font-size "18px"}])))
+           (is (= "funny-selector:lang(urr) > b{font-size:18px}"
+                  (garden->css ["funny-selector:lang(urr) > b" {:font-size "18px"}])))
+           (is (= "@media screen{h1{a:b}}" (garden->css [(at-media {:screen true} [:h1 {:a :b}])])))))
 
 (deftest at-media-test
   (testing "garden's own @media features"
-           (are [css media]
-                (= css (garden->css (at-media media [])))
-                "@media screen{}" {:screen true}
-                "@media not screen{}" {:screen false}
-                "@media screen and (min-width:500px){}" {:screen true,
-                                                         :min-width "500px"}
-                "@media screen,not braille{}" [{:screen true} {:braille false}]
-                "@media only screen{}" {:screen :only}
-                "@media(min-width:768px) and (max-width:959px){}"
-                  {:min-width (px 768), :max-width (px 959)})))
+           (is (= "@media screen{}" (garden->css (at-media {:screen true} []))))
+           (is (= "@media not screen{}" (garden->css (at-media {:screen false} []))))
+           (is (= "@media(min-width:500px) and screen{}" (garden->css (at-media {:min-width "500px" :screen true} []))))
+           (is (= "@media screen,not braille{}" (garden->css (at-media [{:screen true} {:braille false}] []))))
+           (is (= "@media only screen{}" (garden->css (at-media {:screen :only} []))))
+           (is (= "@media(max-width:959px) and (min-width:768px){}"
+                  (garden->css (at-media {:max-width (px 959) :min-width (px 768)} []))))))
