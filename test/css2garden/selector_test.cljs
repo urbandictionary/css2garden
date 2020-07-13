4f1477b6ef59d81c0f52720622484ea56bf2f51a
Ivan Kuznetsov <me@jeiwan.ru>
2020-07-13 10:59:53 +0700

    Convert to table tests

--- test/css2garden/selector_test.cljs
+++ test/css2garden/selector_test.cljs
@@ -1,160 +1,82 @@
 (ns css2garden.selector-test
   (:require [css2garden.selector :refer [ast->garden]]
-            [clojure.test :refer [deftest is are testing]]
+            [clojure.test :refer [deftest are testing]]
             [garden.selectors :as s]))
 
 (deftest ast->garden-test
-  (testing "star selector"
-           (is (= [[:* {:font-weight "bold"}]]
-                  (ast->garden "*" {:font-weight "bold"}))))
-  (testing "combined star selector"
-           (is (= [[:* [:* {:font-weight "bold"}]]]
-                  (ast->garden "* *" {:font-weight "bold"}))))
-  (testing "tag selector"
-           (is (= [[:h1 {:font-weight "bold"}]]
-                  (ast->garden "h1" {:font-weight "bold"}))))
-  (testing "combined tag selector"
-           (is (= [[:h1 [:div {:font-size 12}]]]
-                  (ast->garden "h1 div" {:font-size 12}))))
-  (testing "class selector"
-           (is (= [[:.bold {:font-weight "bold"}]]
-                  (ast->garden ".bold" {:font-weight "bold"}))))
-  (testing "combined class selector"
-           (is (= [[:.text [:.bold {:font-weight "bold"}]]]
-                  (ast->garden ".text .bold" {:font-weight "bold"}))))
-  (testing "id selector"
-           (is (= [[:#bold {:font-weight "bold"}]]
-                  (ast->garden "#bold" {:font-weight "bold"}))))
-  (testing "combined id selector"
-           (is (= [[:#text [:#bold {:font-weight "bold"}]]]
-                  (ast->garden "#text #bold" {:font-weight "bold"}))))
-  (testing "direct child combinator"
-           (is (= [[:h1 [:&>span {:color "#fae"}]]]
-                  (ast->garden "h1 > span" {:color "#fae"}))))
-  (testing "adjacent combinator"
-           (is (= [[:h1 [:&+span {:color "#fae"}]]]
-                  (ast->garden "h1 + span" {:color "#fae"}))))
-  (testing "sibling combinator"
-           (is (= [[:h1 ["~span" {:color "#fae"}]]]
-                  (ast->garden "h1 ~ span" {:color "#fae"}))))
-  (testing "mixed selector"
-           (is (= [[:#block [:a [:&+b [:&>span [:.highlight {:color "red"}]]]]]]
-                  (ast->garden "#block a + b > span .highlight"
-                               {:color "red"}))))
-  (testing "pseudo-classes"
-           (is (= [[":active" {:color "red"}]]
-                  (ast->garden ":active" {:color "red"})))
-           (is (= [[:*:active {:color "red"}]]
-                  (ast->garden "*:active" {:color "red"})))
-           (is (= [[:a:active {:color "red"}]]
-                  (ast->garden "a:active" {:color "red"})))
-           (is (= [[:body [:#container [:a:active {:color "red"}]]]]
-                  (ast->garden "body #container a:active" {:color "red"})))
-           (is (= [[:form [:input:checked [:&+label {:color "red"}]]]]
-                  (ast->garden "form input:checked + label" {:color "red"})))
-           (is (= [[:body [:p:nth-child {:color "red"}]]]
-                  (ast->garden "body p:nth-child" {:color "red"}))))
-  (testing "pseudo-elements"
-           (is (= [[(keyword "a::after") {:color "red"}]]
-                  (ast->garden "a::after" {:color "red"})))
-           (is (= [[(keyword "*::after") {:color "red"}]]
-                  (ast->garden "*::after" {:color "red"})))
-           (is (= [[:a:after {:color "red"}]]
-                  (ast->garden "a:after" {:color "red"})))
-           (is (= [[(keyword "a::first-line") {:color "red"}]]
-                  (ast->garden "a::first-line" {:color "red"})))
-           (is (= [[(keyword "a::-moz-selection") {:color "red"}]]
-                  (ast->garden "a::-moz-selection" {:color "red"}))))
-  (testing
-    "attribute selectors"
-    (is (= [["[a=\"b\"]" {:color "red"}]]
-           (ast->garden "[a=\"b\"]" {:color "red"})))
-    (is (= [["[id]" {:color "red"}]] (ast->garden "[id]" {:color "red"})))
-    (is (= [[:#test {:color "red"}]]
-           (ast->garden "[id=\"test\"]" {:color "red"})))
-    (is (= [[:a#test {:color "red"}]]
-           (ast->garden "a[id=\"test\"]" {:color "red"})))
-    (is (= [["[id$=\"test\"]" {:color "red"}]]
-           (ast->garden "[id$=\"test\"]" {:color "red"})))
-    (is (= [["[id|=\"test\"]" {:color "red"}]]
-           (ast->garden "[id|=\"test\"]" {:color "red"})))
-    (is (= [["[id^=\"test\"]" {:color "red"}]]
-           (ast->garden "[id^=\"test\"]" {:color "red"})))
-    (is (= [["[id*=\"test\"]" {:color "red"}]]
-           (ast->garden "[id*=\"test\"]" {:color "red"})))
-    (is (= [["[class]" {:color "red"}]] (ast->garden "[class]" {:color "red"})))
-    (is (= [[:.test {:color "red"}]]
-           (ast->garden "[class=\"test\"]" {:color "red"})))
-    (is (= [[:a.test {:color "red"}]]
-           (ast->garden "a[class=\"test\"]" {:color "red"})))
-    (is (= [["[class$=\"test\"]" {:color "red"}]]
-           (ast->garden "[class$=\"test\"]" {:color "red"})))
-    (is (= [["[class|=\"test\"]" {:color "red"}]]
-           (ast->garden "[class|=\"test\"]" {:color "red"})))
-    (is (= [["[class^=\"test\"]" {:color "red"}]]
-           (ast->garden "[class^=\"test\"]" {:color "red"})))
-    (is (= [["[class*=\"test\"]" {:color "red"}]]
-           (ast->garden "[class*=\"test\"]" {:color "red"})))
-    (is (= [["*[a=\"b\"]" {:color "red"}]]
-           (ast->garden "*[a=\"b\"]" {:color "red"})))
-    (is (= [[:form ["input[type=\"text\"]" {:color "red"}]]]
-           (ast->garden "form input[type=\"text\"]" {:color "red"})))
-    (is (= [["a[src~=\"https\"]" {:color "red"}]]
-           (ast->garden "a[src~=\"https\"]" {:color "red"})))
-    (is (= [["a[src|=\"https\"]" {:color "red"}]]
-           (ast->garden "a[src|=\"https\"]" {:color "red"})))
-    (is (= [["a[src^=\"https\"]" {:color "red"}]]
-           (ast->garden "a[src^=\"https\"]" {:color "red"})))
-    (is (= [["a[src$=\"https\"]" {:color "red"}]]
-           (ast->garden "a[src$=\"https\"]" {:color "red"})))
-    (is (= [["a[src*=\"https\"]" {:color "red"}]]
-           (ast->garden "a[src*=\"https\"]" {:color "red"}))))
-  (testing
-    "mixed pseudo-classes, elements, attributes"
-    (is (= [[:a:active:hover {:color "#f00"}]]
-           (ast->garden "a:active:hover" {:color "#f00"})))
-    (is (= [[:a:active:hover:focus {:color "#f00"}]]
-           (ast->garden "a:active:hover:focus" {:color "#f00"})))
-    (is (= [[:a:active:hover:focus:first {:color "#f00"}]]
-           (ast->garden "a:active:hover:focus:first" {:color "#f00"})))
-    (is (= [["a[attr=\"test\"]:hover" {:color "#f00"}]]
-           (ast->garden "a[attr=\"test\"]:hover" {:color "#f00"})))
-    (is (= [["a[attr=\"test\"]:hover::after" {:color "#f00"}]]
-           (ast->garden "a[attr=\"test\"]:hover::after" {:color "#f00"})))
-    (is
-      (=
-        [["a[attr=\"test\"]:hover::after"
-          ["b[attr=\"ud\"]:focus::before"
-           [(keyword "c:active::after") {:color "#f00"}]]]]
-        (ast->garden
-          "a[attr=\"test\"]:hover::after b[attr=\"ud\"]:focus::before
-          c:active::after"
-          {:color "#f00"})))
-    (is (= [["[attr=\"test\"]:hover::after" {:color "#f00"}]]
-           (ast->garden "[attr=\"test\"]:hover::after" {:color "#f00"}))))
-  (testing
-    "multiple selectors"
-    (is (= [[:h1 {:font-weight "bold"}] [:h2 {:font-weight "bold"}]]
-           (ast->garden "h1, h2" {:font-weight "bold"})))
-    (is (= [[:h1 [:a {:font-weight "bold"}]] [:h2 [:b {:font-weight "bold"}]]]
-           (ast->garden "h1 a, h2 b" {:font-weight "bold"})))
-    (is (= [[:h1 {:font-weight "bold"}] [:.bold {:font-weight "bold"}]]
-           (ast->garden "h1, .bold" {:font-weight "bold"})))
-    (is
-      (= [[:h1 [:strong {:font-weight "bold"}]] [:h1 [:b {:font-weight "bold"}]]
-          [:h2 [:strong {:font-weight "bold"}]]
-          [:h2 [:b {:font-weight "bold"}]]]
-         (ast->garden "h1 strong, h1 b, h2 strong, h2 b"
-                      {:font-weight "bold"})))
-    (is (= [["a[b]:c" [:d [:&>e {:color "#f00"}]]]
-            ["f[x=\"y\"]" {:color "#f00"}] [:g {:color "#f00"}]]
-           (ast->garden "a[b]:c d>e,f[x=y],g" {:color "#f00"}))))
-  (testing "pseudo-classes with params"
-           (is (= [["li:nth-child(2n+3)" {:color "#f00"}]]
-                  (ast->garden "li:nth-child(2n+3)" {:color "#f00"})))
-           (is (= [["a:not(.internal)" {:color "#f00"}]]
-                  (ast->garden "a:not(.internal)" {:color "#f00"})))
-           (is (= [[":not(.important.dialog)" {:color "#f00"}]]
-                  (ast->garden ":not(.important.dialog)" {:color "#f00"})))
-           (is (= [["p:lang(it)" {:color "#f00"}]]
-                  (ast->garden "p:lang(it)" {:color "#f00"})))))
+  (are
+    [input want]
+    (= want (ast->garden input {})) ;
+    ; basic cases
+    "*" [[:* {}]]
+    "* *" [[:* [:* {}]]]
+    "h1" [[:h1 {}]]
+    "h1 div" [[:h1 [:div {}]]]
+    ".bold" [[:.bold {}]]
+    ".text .bold" [[:.text [:.bold {}]]]
+    "#bold" [[:#bold {}]]
+    "#text #bold" [[:#text [:#bold {}]]]
+    ; combinators
+    "h1 > span" [[:h1 [:&>span {}]]]
+    "h1 + span" [[:h1 [:&+span {}]]]
+    "h1 ~ span" [[:h1 ["~span" {}]]]
+    "#block a + b > span .highlight" [[:#block
+                                       [:a [:&+b [:&>span [:.highlight {}]]]]]]
+    ; pseudo-classes
+    ":active" [[":active" {}]]
+    "*:active" [[:*:active {}]]
+    "a:active" [[:a:active {}]]
+    "body #container a:active" [[:body [:#container [:a:active {}]]]]
+    "form input:checked + label" [[:form [:input:checked [:&+label {}]]]]
+    "body p:nth-child" [[:body [:p:nth-child {}]]]
+    ; pseudo-classes with parameters
+    "li:nth-child(2n+3)" [["li:nth-child(2n+3)" {}]]
+    "a:not(.internal)" [["a:not(.internal)" {}]]
+    ":not(.important.dialog)" [[":not(.important.dialog)" {}]]
+    "p:lang(it)" [["p:lang(it)" {}]]
+    ; pseudo-elements
+    "a::after" [[(keyword "a::after") {}]]
+    "*::after" [[(keyword "*::after") {}]]
+    "a:after" [[:a:after {}]]
+    "a::first-line" [[(keyword "a::first-line") {}]]
+    "a::-moz-selection" [[(keyword "a::-moz-selection") {}]]
+    ; attributes
+    "[a=\"b\"]" [["[a=\"b\"]" {}]]
+    "[id]" [["[id]" {}]]
+    "[id=\"test\"]" [[:#test {}]]
+    "a[id=\"test\"]" [[:a#test {}]]
+    "[id$=\"test\"]" [["[id$=\"test\"]" {}]]
+    "[id|=\"test\"]" [["[id|=\"test\"]" {}]]
+    "[id^=\"test\"]" [["[id^=\"test\"]" {}]]
+    "[id*=\"test\"]" [["[id*=\"test\"]" {}]]
+    "[class]" [["[class]" {}]]
+    "[class=\"test\"]" [[:.test {}]]
+    "a[class=\"test\"]" [[:a.test {}]]
+    "[class$=\"test\"]" [["[class$=\"test\"]" {}]]
+    "[class|=\"test\"]" [["[class|=\"test\"]" {}]]
+    "[class^=\"test\"]" [["[class^=\"test\"]" {}]]
+    "[class*=\"test\"]" [["[class*=\"test\"]" {}]]
+    "*[a=\"b\"]" [["*[a=\"b\"]" {}]]
+    "form input[type=\"text\"]" [[:form ["input[type=\"text\"]" {}]]]
+    "a[src~=\"https\"]" [["a[src~=\"https\"]" {}]]
+    "a[src|=\"https\"]" [["a[src|=\"https\"]" {}]]
+    "a[src^=\"https\"]" [["a[src^=\"https\"]" {}]]
+    "a[src$=\"https\"]" [["a[src$=\"https\"]" {}]]
+    "a[src*=\"https\"]" [["a[src*=\"https\"]" {}]]
+    "a:active:hover" [[:a:active:hover {}]]
+    "a:active:hover:focus" [[:a:active:hover:focus {}]]
+    "a:active:hover:focus:first" [[:a:active:hover:focus:first {}]]
+    "a[attr=\"test\"]:hover" [["a[attr=\"test\"]:hover" {}]]
+    "a[attr=\"test\"]:hover::after" [["a[attr=\"test\"]:hover::after" {}]]
+    "a[attr=\"test\"]:hover::after b[attr=\"ud\"]:focus::before c:active::after"
+      [["a[attr=\"test\"]:hover::after"
+        ["b[attr=\"ud\"]:focus::before" [(keyword "c:active::after") {}]]]]
+    "[attr=\"test\"]:hover::after" [["[attr=\"test\"]:hover::after" {}]]
+    ; mixed cases
+    "h1, h2" [[:h1 {}] [:h2 {}]]
+    "h1 a, h2 b" [[:h1 [:a {}]] [:h2 [:b {}]]]
+    "h1, .bold" [[:h1 {}] [:.bold {}]]
+    "h1 strong, h1 b, h2 strong, h2 b" [[:h1 [:strong {}]] [:h1 [:b {}]]
+                                        [:h2 [:strong {}]] [:h2 [:b {}]]]
+    "a[b]:c d>e,f[x=y],g" [["a[b]:c" [:d [:&>e {}]]] ["f[x=\"y\"]" {}]
+                           [:g {}]]))
