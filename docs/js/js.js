321cbcbdda7927c555e72eeaf7673db204b9b01d
Ivan Kuznetsov <me@jeiwan.ru>
2020-07-14 09:37:11 +0700

    Prettify output in browser

--- docs/js/js.js
+++ docs/js/js.js
@@ -7,7 +7,7 @@
   input.addEventListener('keyup', function (e) {
     try {
       var css = e.target.value;
-      output.value = css2garden.core.convert(css);
+      output.value = css2garden.core.convert_pretty(css);
     } catch { };
 
   }, false);
--- src/css2garden/core.cljs
+++ src/css2garden/core.cljs
@@ -1,6 +1,7 @@
 (ns css2garden.core
   (:require [css2garden.css :refer [ast->garden]]
             [css2garden.object :refer [ast->clj]]
+            [clojure.pprint :as pp]
             [fs :as fs]
             [postcss :refer [parse]]))
 
@@ -9,11 +10,13 @@
   (-> css
       parse
       ast->clj
-      ast->garden
-      pr-str))
+      ast->garden))
+
+(defn convert-pretty [css] (with-out-str (pp/pprint (convert css))))
 
 (defn main
   [& args]
   (-> (.readFileSync fs js/process.stdin.fd "utf-8")
       convert
+      pr-str
       println))
